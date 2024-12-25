6b0131ce77d4370b7c8e075b844a13faea31e448
benjir bhuyan <benjir_bhuyan@yahoo.com>
2024-12-25 12:45:43 +0800

    uddate sections

--- src/GeneralPages/Department/Department.jsx
+++ src/GeneralPages/Department/Department.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Cover from './Cover';
 import coverpic from '../../assets/medical-banner4.webp';
 import { Helmet } from 'react-helmet-async';
+import Services from '../Home/Services';
 
 const Department = () => {
     return (
@@ -12,6 +13,7 @@ const Department = () => {
                 </title>
             </Helmet>
             <Cover image={coverpic}></Cover>
+            <Services></Services>
         </div>
     );
 };
--- src/GeneralPages/Home/Services.jsx
+++ src/GeneralPages/Home/Services.jsx
@@ -1,24 +1,17 @@
-import React, { useEffect, useState } from "react";
 import Subtitle from "../Banners/Subtitle";
-import { data } from "autoprefixer";
-import { key } from "localforage";
 import ServicesDisplay from "./ServicesDisplay";
+import useServices from "../../hooks/useServices";
 
 const Services = () => {
-  const [services, setServices] = useState([]);
-  useEffect(() => {
-    fetch("service.json")
-      .then((res) => res.json())
-      .then((data) => setServices(data));
-  }, []);
+  const [services] = useServices()
   return (
     <div>
       <Subtitle
         title={"our services"}
         subtitle={"Meet Our Department Heads"}
       ></Subtitle>
-      <div className="lg:grid grid-cols-2 gap-3 content-center">
-        {services?.map((service) => (
+      <div className="lg:grid grid-cols-3 gap-3 content-center">
+        {services.map((service) => (
           <ServicesDisplay
             key={service._id}
             service={service}
